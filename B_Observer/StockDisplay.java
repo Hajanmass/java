f624bbd657292e29661553010b854dd20d54fa04
A.Hajanrajh <139778012+Hajanmass@users.noreply.github.com>
2026-03-24 03:13:17 +0530

    Add StockDisplay class implementing Observer interface

---
+++ B_Observer/StockDisplay.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Q2Observer;
+
+/**
+ *
+ * @author Hajan Mass
+ */
+public class StockDisplay implements Observer {
+    public void update(String message) {
+        System.out.println("Display: " + message);
+    }
+}
