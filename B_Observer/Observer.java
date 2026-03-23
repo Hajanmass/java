c99eb02c7d4346b874f82ad4de542a2839d2f175
A.Hajanrajh <139778012+Hajanmass@users.noreply.github.com>
2026-03-24 03:11:31 +0530

    Create Observer interface for update notifications

---
+++ B_Observer/Observer.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Q2Observer;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author Hajan Mass
+ */
+public interface Observer {
+    void update(String message);
+}
