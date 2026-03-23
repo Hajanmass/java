0a0ceaeb29769a8d712e7d38c664048df79fe396
A.Hajanrajh <139778012+Hajanmass@users.noreply.github.com>
2026-03-24 03:09:28 +0530

    Create Form.java

---
+++ B_AbstractFactory/Form.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Q2AbstractFactory;
+
+/**
+ *
+ * @author Hajan Mass
+ */
+public interface Form {
+    void display();
+}
