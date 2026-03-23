c874e7a542c867395fb3b2d036c25628d5368e53
A.Hajanrajh <139778012+Hajanmass@users.noreply.github.com>
2026-03-24 03:10:16 +0530

    Add UIFactory interface for UI component creation

---
+++ B_AbstractFactory/UIFactory.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Q2AbstractFactory;
+
+/**
+ *
+ * @author Hajan Mass
+ */
+public interface UIFactory {
+    Button createButton();
+    Form createForm();
+}
