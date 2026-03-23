82803cbdc8c33c58263720c69db1c33a321aa3fe
A.Hajanrajh <139778012+Hajanmass@users.noreply.github.com>
2026-03-24 03:05:38 +0530

    Add AdminButton class implementing Button interface

---
+++ B_AbstractFactory/AdminButton.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Q2AbstractFactory;
+
+/**
+ *
+ * @author Hajan Mass
+ */
+public class AdminButton implements Button {
+    public void render() {
+        System.out.println("Admin Button Rendered");
+    }
+}
