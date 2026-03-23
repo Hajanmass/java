814366cbbe3875222e1a21a338d6e0d65c02fa62
A.Hajanrajh <139778012+Hajanmass@users.noreply.github.com>
2026-03-24 03:03:41 +0530

    Implement Singleton design pattern

---
+++ Singleton/Singleton.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.assignment.Singleton;
+
+/**
+ *
+ * @author Hajan Mass
+ */
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {} // Private constructor
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("Singleton Instance");
+    }
+
+}
