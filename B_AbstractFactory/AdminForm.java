5e893bf0833ad9c2d18eade80cca33151155d0ae
A.Hajanrajh <139778012+Hajanmass@users.noreply.github.com>
2026-03-24 03:06:40 +0530

    Add AdminForm class implementing Form interface

---
+++ B_AbstractFactory/AdminForm.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Q2AbstractFactory;
+
+/**
+ *
+ * @author Hajan Mass
+ */
+public class AdminForm implements Form  {
+    public void display() {
+        System.out.println("Admin Form Displayed");
+    }
+}
