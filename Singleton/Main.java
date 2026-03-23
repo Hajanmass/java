2e803cce217d95f3f023922094a4a3a4a73e39b5
A.Hajanrajh <139778012+Hajanmass@users.noreply.github.com>
2026-03-24 03:04:12 +0530

    Add Main class to demonstrate Singleton pattern

---
+++ Singleton/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.assignment.Singleton;
+
+/**
+ *
+ * @author Hajan Mass
+ */
+public class Main {
+    public static void main(String[] args) {
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+
+        obj1.showMessage();
+
+        System.out.println(obj1 == obj2); // true
+    }
+
+}
