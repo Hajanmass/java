f7a8c7425483b37ada910594e126df87f790452b
A.Hajanrajh <139778012+Hajanmass@users.noreply.github.com>
2026-03-24 03:08:54 +0530

    Create CashierForm.java

---
+++ B_AbstractFactory/CashierForm.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Q2AbstractFactory;
+
+/**
+ *
+ * @author Hajan Mass
+ */
+public class CashierForm implements Form {
+    public void display() {
+        System.out.println("Cashier Form Displayed");
+    }
+}
